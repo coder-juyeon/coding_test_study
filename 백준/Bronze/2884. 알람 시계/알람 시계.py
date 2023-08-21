653e3a666b9544aa7d69018c08c4289e76ce8e87
coder-juyeon <122768623+coder-juyeon@users.noreply.github.com>
2023-08-21 17:06:18 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,14 +1,18 @@
-# 분은 60에 현재 분을 더한후 45를 뺌
-# 시간은 더한 분이 60을 넘었을 때 1을 빼주기
+# 입력받은 분과 시간을 합쳐 분으로 만들어주기
+# 거기서 45을 빼고
+# 60으로 나눈 몫은 시간 나머지값은 분으로 해주기
 
 hour, minute = map(int, input().split())
 
-if(minute >= 45):
-    minute = minute - 45
-elif(minute < 45):
-    sum_minute = 60 + minute
-    minute = sum_minute - 45
-    hour = 23 if hour == 0 else hour-1
+time= minute + hour * 60
+early_time = time - 45
 
-# print("%d %d" %(hour, minute))
-print("{} {}".format(hour,minute))
+if(early_time < 0):
+    time = minute + 24 * 60
+    early_time = time - 45
+
+hour = early_time // 60
+minute = early_time % 60
+
+
+print("{} {}".format(hour, minute))
