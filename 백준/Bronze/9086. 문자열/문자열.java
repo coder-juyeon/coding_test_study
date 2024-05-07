d640cf8724b1f29186d34dab608f0baba5c077f6
coder-juyeon <122768623+coder-juyeon@users.noreply.github.com>
2024-05-07 10:43:22 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -6,24 +6,16 @@ import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+    
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] words;
-        String num = br.readLine();
-        int iNum = Integer.parseInt(num);
-        words = new String[iNum];
+        int num = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < iNum; i++) {
+        for (int i = 0; i < num; i++) {
             String word = br.readLine();
-            words[i] = word;
-        }
-
-        for (String string : words) {
-            char first = string.charAt(0);
-            char last = string.charAt(string.length()-1);
-            String result = String.valueOf(first) + String.valueOf(last);
-            bw.write(result);
+            bw.write(word.charAt(0));
+            bw.write(word.charAt(word.length() - 1));
             bw.newLine();
         }
         bw.flush();
